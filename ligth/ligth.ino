41b13548a2dc240e54527abf7cac370fc9b4f7bf
Langer <bruno.langer@erzinger.com.br>
2022-02-09 15:11:17 -0300

    Added DMX lib and dmx logics

--- ligth/ligth.ino
+++ ligth/ligth.ino
@@ -1,6 +1,7 @@
 #include <WebServer.h>
 #include <WiFi.h>
 #include <Preferences.h>
+#include <ESPDMX.h>
 
 Preferences prefs;
 
@@ -8,11 +9,13 @@ const char *ssid = "esp";
 const char *password = "12345678";
 WebServer server(80);
 
+#define UNIVERSE_LENGTH 512
 
-byte buffer[512];
+DMXESPSerial dmx;
 
 void setup()
 {
+  dmx.init();
   prefs.begin("dados", false);
   prefs.end();
   Serial.begin(115200);
@@ -29,13 +32,14 @@ void setup()
   Serial.println(WiFi.localIP());
   Serial.println(WiFi.macAddress());
   server.on("/setColor", setColor);
+  server.on("/data", readData);
   server.begin();
 
 }
 
 void loop()
 {
-
+  dmx.update();
   server.handleClient();
 }
 
@@ -44,11 +48,29 @@ void loop()
 void setColor()
 {
   String universe = server.arg("universe");
-    for (int i = 0; i < universe.length(); i+=2){
-    Serial.println(strtol(universe.substring(i,i+2).c_str(),NULL,16));
-}
-  
- // Serial.println(strtol(.c_str(),NULL,16));
+  for (int i = 1; i <= universe.length() + 1; i += 2) {
+    dmx.write(i / 2, strtol(universe.substring(i, i + 2).c_str(), NULL, 16));
+  }
+
+  // Serial.println(strtol(.c_str(),NULL,16));
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/html", "ok");
+}
+
+void readData()
+{
+
+  String sentData = "";
+  for (int i = 1; i <= UNIVERSE_LENGTH + 1; i++) {
+    const uint8_t channelData = dmx.read(i);
+    if(channelData < 10){
+      sentData.concat("0" + String(channelData, HEX));
+      }
+      else{
+        sentData.concat(String(channelData, HEX));
+      }
+    
+  }
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.send(200, "text/html", sentData);
 }
